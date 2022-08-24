df96f451098f9728b9eaa4c40fe7e0ceee074529
vikical <75608575+vikical@users.noreply.github.com>
2022-08-24 11:03:02 +0200

    Update all_executions.sh

--- dnn_examples/all_executions.sh
+++ dnn_examples/all_executions.sh
@@ -1,9 +1,8 @@
+echo "Starting... CIFAR10"
+python CIFAR10_comparison_10_executions.py | tee -a CIFAR10_results.txt
+echo "Starting... MNIST"
+python MNIST_comparison_10_executions.py | tee -a MNIST_results.txt
 echo "Starting... GERMAN"
 python GERMAN_comparison_10_executions.py | tee -a GERMAN_results.txt
 echo "Starting... SHOPPING"
 python SHOPPING_comparison_10_executions.py | tee -a SHOPPING_results.txt
-echo "Starting... MNIST"
-python MNIST_comparison_10_executions.py | tee -a MNIST_results.txt
-echo "Starting... CIFAR10"
-python CIFAR10_comparison_10_executions.py | tee -a CIFAR10_results.txt
-
